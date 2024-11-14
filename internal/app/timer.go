e2afc465c61461cab7ccf57c7efe948aef0b6e84
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-11-14 14:15:42 +0300

    добавлен динамический текст уведомления и вывод сообщений в терминал

--- internal/app/timer.go
+++ internal/app/timer.go
@@ -12,11 +12,13 @@ import (
 	"github.com/algrvvv/pomodoro/internal/config"
 	"github.com/algrvvv/pomodoro/internal/notify"
 	"github.com/algrvvv/pomodoro/internal/types"
+	"github.com/algrvvv/pomodoro/internal/utils"
 )
 
 var (
 	sessionCount = 0
 	sessionType  = types.WorkSession
+	notifier     notify.Notifier
 )
 
 func saveTime(t *string) {
@@ -25,26 +27,36 @@ func saveTime(t *string) {
 	}
 
 	if sessionCount == config.Config.SessionsGoal {
-		fmt.Println("\n\nGOAL\n\n")
+		// TODO: потом отслеживать в бд, чтобы не повторяться
+		fmt.Print(notify.GoalMessage)
+		notifier.Notify(
+			"You have reached your daily goal!",
+			fmt.Sprintf("You have already held %d working sessions today", sessionCount),
+		)
 	}
-	fmt.Printf("[%d] save working time: %v for %v", sessionCount, *t, sessionType)
+	// fmt.Printf("\n\n[%d] save working time: %v for %v\n\n", sessionCount, *t, sessionType)
 }
 
 func Start(ctx context.Context, n notify.Notifier, wg *sync.WaitGroup) error {
 	defer wg.Done()
 	scanner := bufio.NewScanner(os.Stdin)
+	notifier = n
 
 	for {
 		if sessionType == types.WorkSession {
+			utils.ClearTerminal()
+			fmt.Print(notify.BackToWork)
+
 			workDuration := time.Duration(config.Config.WorkMinutes) * time.Second
-			fmt.Println("start work")
-			if err := startTimer(ctx, workDuration, n); err != nil {
+			if err := startTimer(ctx, workDuration); err != nil {
 				return err
 			}
-			fmt.Println("\n\nend work")
 
 			sessionType = types.BreakSession
 		} else if sessionType == types.BreakSession {
+			utils.ClearTerminal()
+			fmt.Print(notify.BreakTime)
+
 			var breakDuration time.Duration
 
 			if sessionCount%config.Config.BreakAfterSessions == 0 && sessionCount != 0 {
@@ -53,16 +65,14 @@ func Start(ctx context.Context, n notify.Notifier, wg *sync.WaitGroup) error {
 				breakDuration = time.Duration(config.Config.ShortBreakMinutes) * time.Second
 			}
 
-			fmt.Println("start break")
-			if err := startTimer(ctx, breakDuration, n); err != nil {
+			if err := startTimer(ctx, breakDuration); err != nil {
 				return err
 			}
-			fmt.Println("\n\nend break")
 
 			sessionType = types.WorkSession
 		}
 
-		fmt.Println("\n\npress Enter to continue...")
+		fmt.Println("\n\npress Enter to continue or write q to quit...")
 		if scanner.Scan() {
 			line := scanner.Text()
 			if line == "q" || line == "quit" {
@@ -77,10 +87,25 @@ func Start(ctx context.Context, n notify.Notifier, wg *sync.WaitGroup) error {
 	}
 }
 
-func startTimer(ctx context.Context, duration time.Duration, n notify.Notifier) error {
+func startTimer(ctx context.Context, duration time.Duration) error {
 	var timeDiff string
 	defer saveTime(&timeDiff)
-	defer n.Notify("Title", "Some message")
+	defer func(diff *string) {
+		var title, message string
+		if sessionType == types.WorkSession {
+			// TODO: проверить чтобы при раннем отключении помидора все равно выводилось сообщение
+			// со временем проведенным за работой
+			title = "It's time to take a break!"
+			message = fmt.Sprintf("Your working session #%d lasted %s", sessionCount+1, *diff)
+		} else if sessionType == types.BreakSession {
+			title = "It's time to get back to work"
+			message = "Start a new work session right now!"
+		}
+
+		if err := notifier.Notify(title, message); err != nil {
+			fmt.Println("failed to notify: ", err)
+		}
+	}(&timeDiff)
 
 	start := time.Now()
 	c, cancel := context.WithTimeout(ctx, duration)
@@ -100,9 +125,9 @@ func startTimer(ctx context.Context, duration time.Duration, n notify.Notifier) 
 				seconds := int(dif.Seconds()) % 60
 				timeDiff = fmt.Sprintf("%02d:%02d:%02d", hours, minutes, seconds)
 
-				fmt.Printf("\rTime worked: %s", timeDiff)
+				fmt.Printf("\rTime passed: %s", timeDiff)
 			case <-c.Done():
-				fmt.Println("\n\ngot interrupt. returned")
+				fmt.Print("\n\n\n\n")
 				return
 			}
 		}
