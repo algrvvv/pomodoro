414df5f493120ce9d59178b4aea99ca17e2f5ac7
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-11-14 18:27:37 +0300

    добавлено подключение бд и использование репозитория

--- cmd/pomodoro/main.go
+++ cmd/pomodoro/main.go
@@ -9,7 +9,9 @@ import (
 
 	"github.com/algrvvv/pomodoro/internal/app"
 	"github.com/algrvvv/pomodoro/internal/config"
+	"github.com/algrvvv/pomodoro/internal/database"
 	"github.com/algrvvv/pomodoro/internal/notify"
+	repos "github.com/algrvvv/pomodoro/internal/repositories/postgres"
 )
 
 func main() {
@@ -17,14 +19,21 @@ func main() {
 		fmt.Println("failed to load config")
 		os.Exit(1)
 	}
+	config.Config.Pomodoro.SessionGoalMinutes = config.Config.Pomodoro.WorkMinutes * config.Config.Pomodoro.SessionsGoal
+
+	if err := database.Connect(); err != nil {
+		fmt.Println("database connection failed: ", err)
+	}
+	defer database.Close()
 
 	wg := sync.WaitGroup{}
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 
 	wg.Add(1)
 	notifier := notify.GetTerminalNotifier()
+	sessionRepo := repos.NewPostgresRepo()
 
-	if err := app.Start(ctx, notifier, &wg); err != nil {
+	if err := app.Start(ctx, notifier, sessionRepo, &wg); err != nil {
 		fmt.Println("app error: ", err)
 		os.Exit(0)
 	}
