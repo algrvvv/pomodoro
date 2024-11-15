5fb0dab016f7145b85debb18faa884be5cd91d6d
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-11-15 14:15:47 +0300

    добавлено создание веб сервера

---
+++ internal/server/server.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/algrvvv/pomodoro/internal/config"
+	"github.com/algrvvv/pomodoro/internal/repositories"
+	"github.com/algrvvv/pomodoro/internal/server/handlers"
+)
+
+func NewServer(r repositories.SessionRepository) *http.Server {
+	s := http.NewServeMux()
+
+	s.HandleFunc("GET /api/v1/data", handlers.GetData(r))
+
+	// статический роут для клиентских файлов
+	s.Handle("/", http.FileServer(http.Dir("./static")))
+
+	return &http.Server{
+		Addr:    ":" + config.Config.App.Port,
+		Handler: s,
+	}
+}
